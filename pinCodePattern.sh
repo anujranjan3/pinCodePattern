4e5777865aded1549536927a742e4d464bbc47a2
unknown <ararky7@gmail.com>
2020-10-13 19:48:18 +0530

    Regex pattern to validate pin 400088

--- pinCodePattern.sh
+++ pinCodePattern.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 
-echo "Welcome to Pin Code Pattern Matching Program"
+read -p "Enter Pincode : " pincode;
+
+pinPattern="^[0-9]{6}$"
+
+if [[ $pincode =~ $pinPattern ]]
+then
+	echo $pincode" is Valid";
+else
+	echo $pincode" is invalid";
+fi
